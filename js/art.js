05e1687471714d3941753d8f895401e66050f77e
jxgzs <52615372+jxgzs@users.noreply.github.com>
2022-12-28 16:50:23 -0500

    Update art.js

--- js/art.js
+++ js/art.js
@@ -298,7 +298,7 @@ var EC = {
             vide_init.prepend('<div class="ec-logo"><img src="'+config.logo+'"></div>');
         }
         if(config.marquee.length  > 2){
-            vide_init.prepend('<div class="marquee"><marquee class="ec-tyy">'+config.marquee+'</marquee></div>');
+            vide_init.prepend('<div class="marquee"><marquee loop="1" class="ec-tyy">'+config.marquee+'</marquee></div>');
         }
         vide_init.prepend('<div class="vodlist-of danmu-hide"></div><div class="ec-listbox"><div class="anthology-wrap"></div></div></div><div class="r-button"><span class="yzmplayer-icon-content"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path d="M448 128a106.667 106.667 0 0 1 106.667 106.667v576A106.667 106.667 0 0 1 448 917.333H128A106.667 106.667 0 0 1 21.333 810.667v-576A106.667 106.667 0 0 1 128 128h320z m448 256a106.667 106.667 0 0 1 106.667 106.667v320A106.667 106.667 0 0 1 896 917.333H661.333a42.667 42.667 0 1 1 0-85.333H896a21.333 21.333 0 0 0 21.333-21.333v-320A21.333 21.333 0 0 0 896 469.333H661.333a42.667 42.667 0 1 1 0-85.333zM448 213.333H128a21.333 21.333 0 0 0-21.333 21.334v554.666A21.333 21.333 0 0 0 128 810.667h320a21.333 21.333 0 0 0 21.333-21.334V234.667A21.333 21.333 0 0 0 448 213.333zM384 672a32 32 0 0 1 0 64H213.333a32 32 0 0 1 0-64z"></path></svg></span></div>');
         $(document).on('click', '.vodlist-of', function() {
